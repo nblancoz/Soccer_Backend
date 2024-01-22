51c43b4a1245d36cc6a5c62d763ecb74a8b9a172
nblancoz <nblancozappala@gmail.com>
2024-01-22 13:46:54 +0100

    initialization project done

---
+++ config/config.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Database connected succesfully");
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error connecting with the database");
+  }
+};
+
+module.exports = {
+  dbConnection,
+};
---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const PORT = process.env.PORT || 3001;
+const cors = require("cors");
+
+const { dbConnection } = require("./config/config");
+dbConnection();
+
+app.use(express.json());
+app.use(cors());
+
+// app.use("/teams", require("./routes/teams"));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
